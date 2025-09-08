944cefee282e768e4c416ea7112119318d143b10
TongZhou1210 <zhoutongzt1210@gmail.com>
2025-09-08 16:32:42 -0400

    test
    test

---
+++ Arrays.java
@@ -0,0 +1,40 @@
+// Exercise Arrays.java
+public class Arrays {
+   public static void main(String[] args) {
+      int[] counts = new int[5];
+      int[] bonus = new int[6];
+      int[] bestScores = new int[11];
+      //Write statements that perform the following one-dimensional-array operations:
+      // a) Set the 10 elements of integer array counts to zero.
+      for (int u = 0; u < counts.length; u++) {
+         //TODO
+      }
+
+      // b) Add one to each of the 15 elements of integer array bonus.
+      for (int v = 0; v < bonus.length; v++) {
+         //TODO
+      }
+      
+      // c) Display the five values of integer array bestScores in column format.
+      for (int w = 0; w < bestScores.length; w++) {
+    	  //TODO: Complete the println statement below as //TODO
+         System.out.println( );
+      }
+   } 
+}
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
